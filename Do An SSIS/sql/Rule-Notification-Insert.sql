b5b7e7c4f7c0bb411320f23c48699501553efef1
MageOfFlowers <100255997+MageOfFlowers@users.noreply.github.com>
2024-11-22 15:40:25 +0700

    Update Rule-Notification-Insert.sql

--- Do An SSIS/sql/Rule-Notification-Insert.sql
+++ Do An SSIS/sql/Rule-Notification-Insert.sql
@@ -5,7 +5,8 @@ VALUES
 ('Rule 3', 'Check data length', 1, 3, 2, 2, 2, GETDATE(), GETDATE()),
 ('Rule 4', 'Validate numeric ranges', 1, 1, 3, 1, 1, GETDATE(), GETDATE()),
 ('Rule 5', 'Match data types', 2, 2, 2, 2, 2, GETDATE(), GETDATE()),
-('Rule 6', 'Check date consistency', 1, 3, 1, 3, 3, GETDATE(), GETDATE())
+('Rule 6', 'Check date consistency', 1, 3, 1, 3, 3, GETDATE(), GETDATE()),
+('Rule 7', 'Check defining site format', 1, 3, 1, 3, 3, GETDATE(), GETDATE())
 
 INSERT INTO DQNotification_Metadata (DQRuleKey, DQRecepientKey, Method)
 VALUES
